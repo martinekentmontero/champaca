38172f6bb8e83d78f523683f79be93214ec30350
martinekentmontero <mmontero2031@wvc.pshs.edu.ph>
2026-09-14 15:45:24 +0800

    Add files via upload

---
+++ Montero_MartineKent_DistanceProblem.py (1).py
@@ -0,0 +1,10 @@
+import math
+
+x1 = float(input("Enter x1:"))
+x2 = float(input("Enter x2:"))
+y1 = float(input("Enter y1:"))
+y2 = float(input("Enter y2:"))
+
+distance = math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
+
+print ("The distance:", distance)
