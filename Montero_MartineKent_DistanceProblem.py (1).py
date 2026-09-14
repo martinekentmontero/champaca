48f2df3565e637ad728b36aa115e2f620d482909
martinekentmontero <mmontero2031@wvc.pshs.edu.ph>
2026-09-14 15:51:59 +0800

    Implement distance calculation between two points
    Added user input for coordinates and distance calculation.

--- Montero_MartineKent_DistanceProblem.py (1).py
+++ Montero_MartineKent_DistanceProblem.py (1).py
@@ -1,10 +1,14 @@
 import math
 
+# Ask the user to enter the coordinates for the first point.
 x1 = float(input("Enter x1:"))
 x2 = float(input("Enter x2:"))
+# Ask the user to enter the coordinates for the second point.
 y1 = float(input("Enter y1:"))
 y2 = float(input("Enter y2:"))
 
+# Compute the distance using the distance formula
 distance = math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
 
+# Display the distance rounded up to two decimal places
 print ("The distance:", distance)
